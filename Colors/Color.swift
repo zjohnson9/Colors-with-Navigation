d291b9d0df9f7d5c1fa8668f804c6816c9d17845
Zac Johnson <zejc5f@mail.missouri.edu>
2018-10-19 10:43:05 -0500

    Added color struct to set change background color on cells

---
+++ Colors/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by Zac Johnson on 10/19/18.
+//  Copyright © 2018 ZacJohnson. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
--- Colors/ColorListViewController.swift
+++ Colors/ColorListViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class ColorListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    var colors = ["red", "orange", "yellow", "green", "blue", "purple","brown"]
+    //var colors = ["red", "orange", "yellow", "green", "blue", "purple","brown"]
   
+    var colors = [Color(name: "red", uiColor: UIColor.red),
+                  Color(name: "orange", uiColor: UIColor.orange),
+                  Color(name: "yellow", uiColor: UIColor.yellow),
+                  Color(name: "green", uiColor: UIColor.green),
+                  Color(name: "blue", uiColor: UIColor.blue),
+                  Color(name: "purple", uiColor: UIColor.purple),
+                  Color(name: "brown", uiColor: UIColor.brown)]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -34,7 +43,10 @@ class ColorListViewController: UIViewController, UITableViewDataSource, UITableV
     internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell", for: indexPath)
         
-        cell.textLabel?.text = colors[indexPath.row]
+        let color = colors[indexPath.row]
+        
+        cell.textLabel?.text = color.name
+        cell.backgroundColor = color.uiColor
         
         return cell
     }
