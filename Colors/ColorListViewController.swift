0af0e0adda5733f2c14d1d17e88c990e69c19763
Zac Johnson <zejc5f@mail.missouri.edu>
2018-10-19 10:19:49 -0500

    First commit

---
+++ Colors/ColorListViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ColorListViewController.swift
+//  Colors
+//
+//  Created by Zac Johnson on 9/28/18.
+//  Copyright © 2018 ZacJohnson. All rights reserved.
+//
+
+import UIKit
+
+class ColorListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var colors = ["red", "orange", "yellow", "green", "blue", "purple","brown"]
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell", for: indexPath)
+        
+        cell.textLabel?.text = colors[indexPath.row]
+        
+        return cell
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
